21b0c4a5e6c5f657a7d4891d9d088857c817132d
mateuswetah <mateus.m.luna@gmail.com>
2020-11-17 20:06:56 -0300

    Version 0.0.5: Adds style for multivalue separator.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 echo "Compiling necessary stuff..."
-cd ./tainacan-metadata-type-url/metadata_type/
+cd ./tainacan-url-metadata-type/metadata_type/
 npm install
 npm run build
 cd ../../
@@ -9,14 +9,14 @@ then
     echo "Done!"
 else
     echo "Done. Moving files to destination folder: $1"
-    rm -rf $1/tainacan-metadata-type-url
-    cp -r ./tainacan-metadata-type-url $1
+    rm -rf $1/tainacan-url-metadata-type
+    cp -r ./tainacan-url-metadata-type $1
     echo "Cleaning some files not necessary for the plugin to work..."
-    rm -f $1/tainacan-metadata-type-url/metadata_type/webpack.config.js
-    rm -f $1/tainacan-metadata-type-url/metadata_type/package.json
-    rm -f $1/tainacan-metadata-type-url/metadata_type/package-lock.json
-    rm -f $1/tainacan-metadata-type-url/metadata_type/*.vue
-    rm -f $1/tainacan-metadata-type-url/metadata_type/metadata-type.js
-    rm -rf $1/tainacan-metadata-type-url/metadata_type/node_modules
+    rm -f $1/tainacan-url-metadata-type/metadata_type/webpack.config.js
+    rm -f $1/tainacan-url-metadata-type/metadata_type/package.json
+    rm -f $1/tainacan-url-metadata-type/metadata_type/package-lock.json
+    rm -f $1/tainacan-url-metadata-type/metadata_type/*.vue
+    rm -f $1/tainacan-url-metadata-type/metadata_type/metadata-type.js
+    rm -rf $1/tainacan-url-metadata-type/metadata_type/node_modules
     echo "Done!"
 fi
---
+++ tainacan-url-metadata-type/tainacan-metadata-type-url.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Plugin Name: Tainacan URL Metadata Type
+Plugin URI: https://tainacan.org/
+Description: A URL Metadata Type for Tainacan, that displays a link or an embed content, if possible.
+Author: tainacan
+Version: 0.0.5
+Text Domain: tainacan-metadata-type-url
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+/** Plugin version */
+const TAINACAN_URL_PLUGIN_VERSION = '0.0.5';
+
+add_action("tainacan-register-metadata-type", "tainacan_url_plugin_register_metadata_type");
+function tainacan_url_plugin_register_metadata_type($helper) {
+
+    // Registering the Class
+    require_once( plugin_dir_path(__FILE__) . 'metadata_type/metadata-type.php' );
+
+    // Registering the Vue Component
+    $handle = 'tainacan-metadata-type-url';
+    $class_name = 'TAINACAN_URL_Plugin_Metadata_Type';
+    $metadata_script_url = plugin_dir_url(__FILE__) . 'metadata_type/metadata-type.bundle.js';
+    $helper->register_metadata_type($handle, $class_name, $metadata_script_url);
+}
+
+add_action("tainacan-register-vuejs-component", "tainacan_url_plugin_register_metadata_type_form");
+function tainacan_url_plugin_register_metadata_type_form($helper) {
+
+    // Registering the Vue Component for the Metadata Options Form
+    $handle2 = 'url-metadata-type-form';
+    $component_script_url = plugin_dir_url(__FILE__) . 'metadata_type/metadata-type-form.js';
+    $helper->register_vuejs_component($handle2, $component_script_url);
+
+}
+
+add_action("wp_enqueue_scripts", "tainacan_url_plugin_enqueue_styles");
+function tainacan_url_plugin_enqueue_styles() {
+
+    // Registers the plugin necessary style to display some of URL metadata type content
+	wp_register_style( 'TainacanURLPluginStyle', plugin_dir_url(__FILE__) . 'metadata_type/metadata-type.css', '', TAINACAN_URL_PLUGIN_VERSION );
+	wp_enqueue_style( 'TainacanURLPluginStyle' );
+}
+?>
