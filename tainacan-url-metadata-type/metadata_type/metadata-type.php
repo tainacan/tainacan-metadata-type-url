27fd53a0bbefd80790cdf2e697920c913d8f7e25
vnmedeiros <vnicius.nm.ba@gmail.com>
2021-11-12 10:56:07 -0300

    adds markdown link format

--- tainacan-url-metadata-type/metadata_type/metadata-type.php
+++ tainacan-url-metadata-type/metadata_type/metadata-type.php
@@ -5,50 +5,50 @@
  * This is inside "metadata_type/metadata-type.php"
  */
 class TAINACAN_URL_Plugin_Metadata_Type extends \Tainacan\Metadata_Types\Metadata_Type {
-    use \Tainacan\Traits\Formatter_Text;
+	use \Tainacan\Traits\Formatter_Text;
 
-    function __construct() {
+	function __construct() {
 
-        parent::__construct();
+		parent::__construct();
 
-        // Basic options
-        $this->set_name( __('URL Metadata Type', 'tainacan-metadata-type-url') );
-        $this->set_description( __('An URL link, possibly with an embed content.', 'tainacan-metadata-type-url') );
-        $this->set_primitive_type(['text']);
-        $this->set_component('tainacan-metadata-type-url');
-        $this->set_form_component('tainacan-metadata-form-type-url');
-        $this->set_default_options([
+		// Basic options
+		$this->set_name( __('URL Metadata Type', 'tainacan-metadata-type-url') );
+		$this->set_description( __('An URL link, possibly with an embed content.', 'tainacan-metadata-type-url') );
+		$this->set_primitive_type(['text']);
+		$this->set_component('tainacan-metadata-type-url');
+		$this->set_form_component('tainacan-metadata-form-type-url');
+		$this->set_default_options([
 			'force-iframe' => 'no',
 			'iframe-min-height' => '',
 			'iframe-allowfullscreen' => 'no'
 		]);
-        $this->set_preview_template('
-            <div>
-                <div class="control is-clearfix">
-                    <input type="url" placeholder="https://youtube.com/?v=abc123456" class="input">
-                </div>
-            </div>
-        ');
-    }
-    
-    public function get_form_labels(){
-        return [
-            'force-iframe' => [
-                'title' 	  => __( 'Force iframe', 'tainacan-metadata-type-url' ),
-                'description' => __( 'Force the URL to be displayed in an iframe in case the content is not embeddable by WordPress.', 'tainacan-metadata-type-url' ),
+		$this->set_preview_template('
+			<div>
+				<div class="control is-clearfix">
+					<input type="url" placeholder="https://youtube.com/?v=abc123456" class="input">
+				</div>
+			</div>
+		');
+	}
+	
+	public function get_form_labels(){
+		return [
+			'force-iframe' => [
+				'title' 	  => __( 'Force iframe', 'tainacan-metadata-type-url' ),
+				'description' => __( 'Force the URL to be displayed in an iframe in case the content is not embeddable by WordPress.', 'tainacan-metadata-type-url' ),
 			],
 			'iframe-min-height' => [
-                'title' 	  => __( 'Forced iframe minimum height', 'tainacan-metadata-type-url' ),
-                'description' => __( 'If forcing the use of an iframe, sets the height attribute, in pixels. Leave it empty to be 100% of the container.', 'tainacan-metadata-type-url' ),
+				'title' 	  => __( 'Forced iframe minimum height', 'tainacan-metadata-type-url' ),
+				'description' => __( 'If forcing the use of an iframe, sets the height attribute, in pixels. Leave it empty to be 100% of the container.', 'tainacan-metadata-type-url' ),
 			],
 			'iframe-allowfullscreen' => [
-                'title' 	  => __( 'Allow fullscreen on forced iframe', 'tainacan-metadata-type-url' ),
-                'description' => __( 'If forcing the use of an iframe, allows it to request fullscreen to the browser.', 'tainacan-metadata-type-url' ),
-            ]
-        ];
-    }
+				'title' 	  => __( 'Allow fullscreen on forced iframe', 'tainacan-metadata-type-url' ),
+				'description' => __( 'If forcing the use of an iframe, allows it to request fullscreen to the browser.', 'tainacan-metadata-type-url' ),
+			]
+		];
+	}
 
-    /**
+	/**
 	 * Get the value as a HTML string with links
 	 * @return string
 	 */
@@ -74,10 +74,11 @@ class TAINACAN_URL_Plugin_Metadata_Type extends \Tainacan\Metadata_Types\Metadat
 						if (!empty($this->get_option('iframe-min-height')))
 							$iframeMininumHeight = $this->get_option('iframe-min-height');
 
-                        $return .= '<iframe src="' . $el . '" width="100%" height="' . $iframeMininumHeight  . '" style="border:none;" allowfullscreen="' . ($this->get_option('iframe-allowfullscreen') == 'yes' ? 'true' : 'false') . '"></iframe>';
-                    } else {
-                        $return .= $this->make_clickable_links($el);
-                    }
+						$return .= '<iframe src="' . $el . '" width="100%" height="' . $iframeMininumHeight  . '" style="border:none;" allowfullscreen="' . ($this->get_option('iframe-allowfullscreen') == 'yes' ? 'true' : 'false') . '"></iframe>';
+					} else {
+						$mkstr = preg_replace('/\[([^\]]+)\]\(([^\)]+)\)/', '<a href="\2">\1</a>', $el);
+						$return .= $this->make_clickable_links($mkstr);
+					}
 				} else {
 					$return .= $embed;
 				}
@@ -94,15 +95,16 @@ class TAINACAN_URL_Plugin_Metadata_Type extends \Tainacan\Metadata_Types\Metadat
 			$embed = $wp_embed->autoembed($value);
 			
 			if ( $embed == $value ) {
-                if ($this->get_option('force-iframe') == 'yes') {
-                    $iframeMininumHeight = '100%';
+				if ($this->get_option('force-iframe') == 'yes') {
+					$iframeMininumHeight = '100%';
 					if (!empty($this->get_option('iframe-min-height')))
 						$iframeMininumHeight = $this->get_option('iframe-min-height');
 
 					$return = '<iframe src="' . $value . '" width="100%" height="' . $iframeMininumHeight  . '" style="border:none;" allowfullscreen="' . ($this->get_option('iframe-allowfullscreen') == 'yes' ? 'true' : 'false') . '"></iframe>';
-                } else {
-                    $return = $this->make_clickable_links($value);
-                }
+				} else {
+					$value = preg_replace('/\[([^\]]+)\]\(([^\)]+)\)/', '<a href="\2">\1</a>', $value);
+					$return = $this->make_clickable_links($value);
+				}
 			} else {
 				$return = $embed;
 			}
